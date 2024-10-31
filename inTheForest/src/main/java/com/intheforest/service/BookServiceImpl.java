d938a7bf1575db263378d15ddfe921f80ae8b49b
admin <admin@DESKTOP-5R7T68E>
2024-10-31 17:00:04 +0900

    add

--- inTheForest/src/main/java/com/intheforest/service/BookServiceImpl.java
+++ inTheForest/src/main/java/com/intheforest/service/BookServiceImpl.java
@@ -14,7 +14,6 @@ public class BookServiceImpl implements BookService {
 	
 	@Override
 	public List<BookVO> myPageBookList(String memberId) {
-		System.out.println("myPageBookList");
 		return mapper.selectMyPageBookList(memberId);
 	}
 
