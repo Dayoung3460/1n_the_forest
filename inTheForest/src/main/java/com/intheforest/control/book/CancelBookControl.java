683af13b611b30f29b3a110a973293c527a998d0
admin <admin@DESKTOP-MCPJ1C4>
2024-11-06 09:03:23 +0900

    1106_박주현

--- inTheForest/src/main/java/com/intheforest/control/book/CancelBookControl.java
+++ inTheForest/src/main/java/com/intheforest/control/book/CancelBookControl.java
@@ -9,56 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 import com.intheforest.common.Control;
 import com.intheforest.service.BookService;
 import com.intheforest.service.BookServiceImpl;
-import com.intheforest.vo.BookVO;
 
 public class CancelBookControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("utf-8");
-		int siteNo = Integer.parseInt(req.getParameter("siteNo"));
-		String memberId = req.getParameter("memberId");
-		String memCnt = req.getParameter("memCnt");
-		String siteDate = req.getParameter("siteDate");
-		String endDate = req.getParameter("endDate");
-		String carNum = req.getParameter("carNum");
-		String totalPrice = req.getParameter("sitePrice");
-		String[] option = req.getParameterValues("option[]");
-		
-		siteDate = siteDate.replace("-","/");
-		endDate = endDate.replace("-","/");
+		int bookNo = Integer.parseInt(req.getParameter("bookNo"));
 		
 		BookService svc = new BookServiceImpl();
-		BookVO book = new BookVO();
 		
-		book.setSiteNo(siteNo);
-		book.setMemberId(memberId);
-		book.setMemCnt(memCnt);
-		book.setStartDate(siteDate);
-		book.setEndDate(endDate);
-		book.setCarNum(carNum);
-		book.setTotalPrice(totalPrice);
-		
-		try {
-			if(svc.registerBook(book)) {
-				
-				int lastNo = svc.bookLast();
-				System.out.println("option: " +option.length);
-				for(int i=0; i<option.length; i++) {	
-					BookVO bookOption = new BookVO();
-					bookOption.setOptionNo(option[i]);
-					bookOption.setBookNo(lastNo);
-					bookOption.setMemberId(memberId);
-					bookOption.setSiteNo(siteNo);
-					
-					svc.registerBookOption(bookOption);
-				}
-				resp.sendRedirect("/inTheForest/bookCalendar.do");
-			}
-		}catch(Exception e){
-			req.setAttribute("msg","등록하는중 오류가 발생했습니다.");
-			req.getRequestDispatcher("/inTheForest/bookCalendar.do")
-				.forward(req, resp);
+		if(svc.cancelBook(bookNo) >0){
+			// 정상적으로 수정 되면 -> 목록 화면 출력 
+			resp.sendRedirect("main.do");
+		}else {
+			req.setAttribute("msg", "잘못된 값입니다.");
+			req.getRequestDispatcher("main.do").forward(req, resp);
 		}
 	}
 
