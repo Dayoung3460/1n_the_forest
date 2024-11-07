fc0959548e1f4c706b068b4f623c5c1b4b3e2621
Dayoung <kimdayoung771@gmail.com>
2024-11-08 08:50:49 +0900

    수정

--- inTheForest/src/main/java/com/intheforest/mapper/BoardMapper.java
+++ inTheForest/src/main/java/com/intheforest/mapper/BoardMapper.java
@@ -22,7 +22,10 @@ public interface BoardMapper {
   int updateCount(int boardNo);
   
   ArrayList<Integer> getBookNoByMemberId(String memberId);
-
+  
   // 예약번호에 따른 리뷰 조회
   BoardVO reviewOnBook(int bookNo);
+  
+  // 답글이 있는 게시물인지?
+  BoardVO myReply(int boardNo);
 }
